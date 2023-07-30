49e8838c32b95eca24caa36ec7609aa2ff90ae2a
Hamster-Inside <arkadiusz.budkowski.dev@gmail.com>
2023-07-30 11:32:06 +0200

    deleting build from repo and pushing gitignore

--- build/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-D:\Programowanie\ Arduino\2023-07-27-Electric_press_using_stepper\build\core\hooks.c.o: \
- C:\Users\arkad\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\hooks.c
